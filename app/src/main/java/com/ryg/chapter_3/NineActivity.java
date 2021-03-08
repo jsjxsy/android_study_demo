4e8be8268aa19ae5fa0b8fe193b8137f45cb2435
xsy <>
2021-03-08 13:09:17 +0800

    feature: 属性动画

--- app/src/main/java/com/ryg/chapter_3/NineActivity.java
+++ app/src/main/java/com/ryg/chapter_3/NineActivity.java
@@ -2,16 +2,82 @@ package com.ryg.chapter_3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.ryg.chapter_3.ui.MyView;
 
 /**
  * RelativeLayout添加
  * 自定义Drawable实现动画
  */
 public class NineActivity extends Activity {
+    private MyView view;
+    private boolean flag;
+    private int originWidth;
+    private int originHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nine);
+        view = (MyView) findViewById(R.id.view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (flag) {
+                    scaleSmall();
+                    moveView();
+                    flag = false;
+                } else {
+                    if (originHeight == 0 || originWidth == 0) {
+                        getOriginWidthHeight();
+                    }
+                    scaleBig();
+                    moveViewByBig();
+                    flag = true;
+                }
+            }
+        });
+
+    }
+
+    private void getOriginWidthHeight() {
+        originWidth = view.getWidth();
+        originHeight = view.getHeight();
+    }
+
+    private void scaleBig() {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+        view.requestLayout();
+
+    }
+
+    private void scaleSmall() {
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.width = originWidth;
+        layoutParams.height = originHeight;
+        view.requestLayout();
+    }
+
+    private void moveView(){
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        layoutParams.topMargin = 100;
+        layoutParams.rightMargin = 100;
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        view.requestLayout();
+    }
+
+    private void moveViewByBig(){
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+        layoutParams.topMargin = 0;
+        layoutParams.rightMargin = 0;
+        layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_TOP);
+
     }
 }
--- app/src/main/java/com/ryg/chapter_3/SixActivity.java
+++ app/src/main/java/com/ryg/chapter_3/SixActivity.java
@@ -3,17 +3,35 @@ package com.ryg.chapter_3;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.ryg.chapter_3.ui.MyView;
+
 /**
  * 实现属性动画
  * animation和animator
  * 自定义Animation
  */
 public class SixActivity extends Activity {
-
+    private MyView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_six);
+        view = (MyView)findViewById(R.id.view);
+    }
+
+    private void move() {
+
+    }
+
+    private void scale() {
+
+    }
+
+    private void rotate(){
+
+    }
+
+    private void alpha() {
 
     }
 }
